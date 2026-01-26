fc425ae2970b7899dfb69b76a002710a212f829b
divyapardeshi <divyapradeshi@gmail.com>
2026-01-26 10:54:04 +0530

    header page

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import heroImg from './assets/hero-img.png';
+
+import bg from './assets/Back_ground.png';
+import Search from './components/Search';
+import  { useState } from 'react';
+
 
 const App = () => {
+ const [searchTerm, setSearchTerm]=useState('');
+
   return (
-    <div>App</div>
+    <main>
+      
+      <div className="pattern"/>
+     
+    <div className="wrapper">
+      <header>
+          <img src={heroImg} alt="Hero Banner" />
+          
+        <h1 >Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
+      </header>
+       
+       <Search  searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+       
+    </div>
+    </main>
   )
 }
 
---
+++ react_project/src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import search from '../assets/search_icon.svg';
+
+
+const Search = ({searchTerm,setSearchTerm}) => {
+  return (
+    <div className="search">
+      <div>
+        <img src={search} alt="search"/>
+        <input 
+        type="text"
+        placeholder='Search through thousands of movies'
+        value={searchTerm}
+
+
+        onChange={(e) => setSearchTerm(e.target.value)}
+
+       />
+      </div>
+    </div>
+  )
+}
+
+export default Search
